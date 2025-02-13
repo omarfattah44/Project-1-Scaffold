0d557494c5a2bc52deddd66808344bd9bf8e5e71
omarfattah44 <omarmfattah44@gmail.com>
2025-02-13 14:44:14 -0800

    problem statement

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # Project-1-Scaffold
 Productivity/Self-Growth-Application
+
+# Problem Statement:
+Productivity-Organization-and Self-Development go hand and hand.
+
+The problem is I have to use various tools such as planners, to-do lists, calendars, and other various resources to stay ahead of my goals and aspirations.
+
+I need a one stop shop that organizes my life in a way that keeps track of all of my taskers enabling me to be more productive and aid in accomplishing my goals/self-development.
--- scripts/main.js
+++ scripts/main.js
@@ -14,9 +14,9 @@ fetch('https://api.aladhan.com/v1/timingsByCity?city=San%20Luis%20Obispo&country
 function displayPrayerTimes(timings) {
     const prayerTimesContainer = document.getElementById('prayer-times'); // document is the webpage, I'm using getElementbyId to get the element with the id of 'prayer-times'
     // I'm setting the innerHTML of the prayerTimesContainer to the prayer times from the API by using .innerHTML
+    //im using the ${} to insert the prayer times from the API since its a variable and needs to go into a string
     prayerTimesContainer.innerHTML = ` 
     <h2>Prayer Times</h2>
-    <!-- im using the ${} to insert the prayer times from the API since its a variable and needs to go into a string -->
     <p><strong>Fajr:</strong> ${timings.Fajr}</p> 
     <p><strong>Dhuhr:</strong> ${timings.Dhuhr}</p>
     <p><strong>Asr:</strong> ${timings.Asr}</p>
