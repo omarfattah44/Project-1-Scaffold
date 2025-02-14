99609d466907b4ff38ef64abe37d0d0d283d5e79
omarfattah44 <omarmfattah44@gmail.com>
2025-02-14 11:28:26 -0800

    Added Hijri Date

--- scripts/main.js
+++ scripts/main.js
@@ -4,18 +4,34 @@ fetch('https://api.aladhan.com/v1/timingsByCity?city=San%20Luis%20Obispo&country
     .then(data => { // Extracts the prayer times from the API
         const timings = data.data.timings; // setting a constant variable to the data of the prayer times
         console.log(timings); // Check the prayer times in the console
+        const hijriDate = data.data.date.hijri; // setting a constant variable to the data of the hijri date
+        console.log(hijriDate); // Check the hijri date in the console
+        
+        displayIslamicDate(hijriDate); // Calling the function to display the islamic date on the webpage
+
 
         // Display the prayer times on the webpage
         displayPrayerTimes(timings); // Created a function that dynamically adds the prayer times to my webpage
     })
     .catch(error => console.error('Error fetching prayertimes:', error)); // Catch any errors and log them to the console
 
+
+// Function to display the Islamic date on the webpage
+function displayIslamicDate(hijriDate) {
+    const prayerTimesContainer = document.getElementById('prayer-times'); // document is the webpage, I'm using getElementbyId to get the element with the id of 'islamic-date'
+    const islamicDateHTML = `
+    <h3>Hijri Date:</h3>
+     <h3>${hijriDate.weekday.en}, ${hijriDate.day} ${hijriDate.month.en} ${hijriDate.year} AH</h3>
+    `;
+    prayerTimesContainer.insertAdjacentHTML('afterbegin', islamicDateHTML);
+}
+// I'm using the insertAdjacentHTML method to insert the islamic date into the webpage. I'm using the 'afterbegin' parameter to insert the islamic date at the beginning of the prayer times container
+
 // Function to display the prayer times on the webpage
 function displayPrayerTimes(timings) {
     const prayerTimesContainer = document.getElementById('prayer-times'); // document is the webpage, I'm using getElementbyId to get the element with the id of 'prayer-times'
-    // I'm setting the innerHTML of the prayerTimesContainer to the prayer times from the API by using .innerHTML
-    //im using the ${} to insert the prayer times from the API since its a variable and needs to go into a string
-    prayerTimesContainer.innerHTML = ` 
+    //im using the ${} to insert the prayer times from the API since its a variable and needs to go into a string// these are also the API properties inspected from the browser console
+    const prayerTimesContainerHTML = ` 
     <h2>Prayer Times</h2>
     <p><strong>Fajr:</strong> ${timings.Fajr}</p> 
     <p><strong>Dhuhr:</strong> ${timings.Dhuhr}</p>
@@ -23,6 +39,7 @@ function displayPrayerTimes(timings) {
     <p><strong>Maghrib:</strong> ${timings.Maghrib}</p>
     <p><strong>Isha:</strong> ${timings.Isha}</p>
     `;
+    prayerTimesContainer.insertAdjacentHTML('beforeend', prayerTimesContainerHTML); // I'm using the insertAdjacentHTML method to insert the prayer times into the webpage. I'm using the 'beforeend' parameter to insert the prayer times at the end of the prayer times container
 }
 
 // Fetch Quran verse from Quran API
